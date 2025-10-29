8b1042c1314561f158a7bd3596cd5b3ccfec3551
joana1818 <leal.j@acad.ifma.edu.br>
2025-10-29 17:05:45 -0300

    Atualizei o App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import Header from './components/header'
 import Footer from './components/footer'
 import Cadastrar from './pages/formRegister'
 import Dashboard from "./pages/dashboard.tsx"
+import Produtos from "./pages/produtos.tsx"
 
 function App() {
   return (
@@ -12,7 +13,8 @@ function App() {
         <Routes>
           <Route path="/dashboard" element={<Dashboard />} />
           <Route path="/login" element={<FormLogin />} />
-          <Route path="/cadastrar" element={<Cadastrar />} />   
+          <Route path="/cadastrar" element={<Cadastrar />} />
+           <Route path="/produtos" element={<Produtos />} /> 
         </Routes>
       <Footer />
     </>
